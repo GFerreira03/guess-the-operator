42efd4376784145eeb648ac00daf863cb97482ed
Gabriel Ferreira <gabrielferreira1d2015@gmail.com>
2023-01-12 14:32:00 -0300

    corrected to not be able to spam enter

--- controller/RenderQuestion.js
+++ controller/RenderQuestion.js
@@ -1,5 +1,6 @@
 var img;
 let questionOperator;
+let canAnswer;
 window.onload = function () {
   img = document.getElementById('question-img');
   getRandomOperator();
@@ -13,5 +14,6 @@ function getRandomOperator() {
   questionOperator = operator[getRandomInt(operator.length)];
   img.classList.remove('reveal-op');
   img.src = questionOperator.art;
+  canAnswer = true;
   answer.value = "";
 }
