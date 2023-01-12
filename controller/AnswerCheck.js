2669127e573a4756b435da770ea6a1c551dd867e
Gabriel Ferreira <gabrielferreira1d2015@gmail.com>
2023-01-12 14:30:32 -0300

    popup timer

--- controller/AnswerCheck.js
+++ controller/AnswerCheck.js
@@ -1,6 +1,7 @@
 let answer;
 document.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
+  if (e.key === "Enter" && canAnswer == true) {
+    canAnswer = false;
     answer = document.getElementById('answer-input');
     img.classList.add('reveal-op');
     answer.disabled = true;
@@ -12,20 +13,9 @@ document.addEventListener("keypress", function (e) {
       setTimeout(newRound, 3000);
     } else {
       showPopup();
-      setTimeout(resetMatch, 3000)
     }
   }
 });
-
-function showPopup() {
-  document.getElementById('popup').hidden = false;
-  const opName = questionOperator.name.charAt(0).toUpperCase() + questionOperator.name.slice(1);
-  document.getElementById('popup').innerHTML = "Errou... o correto seria " + opName;
-}
-
-function hidePopup() {
-  document.getElementById('popup').hidden = true;
-}
 
 function resetMatch() {
   img.classList.remove('level-2');
---
+++ view/WrongAnswerPopup.js
@@ -0,0 +1,20 @@
+function hidePopup() {
+  const popup = document.getElementById('popup');
+  popup.hidden = true;
+}
+
+function showPopup() {
+  const popup = document.getElementById('popup');
+  popup.hidden = false;
+  const opName = questionOperator.name.charAt(0).toUpperCase() + questionOperator.name.slice(1);
+  popup.innerHTML = "Errou... o correto seria " + opName;
+  let width = 100;
+  let id = setInterval(() => {
+    if (width <= 1) {
+      resetMatch();
+      clearInterval(id);
+    }
+    width -= .1;
+    popup.style.setProperty('--width', width);
+  }, 3);
+}
